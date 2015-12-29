595454fa8d6fabe4150993ef43e883d8e474045e
elers <elers1128@sohu.com>
2015-12-29 15:28:21 +0800

    first commit to local

---
+++ my_first_Git_project/my_first_Git_project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  my_first_Git_project
+//
+//  Created by elers on 15/12/29.
+//  Copyright © 2015年 elers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ my_first_Git_project/my_first_Git_project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  my_first_Git_project
+//
+//  Created by elers on 15/12/29.
+//  Copyright © 2015年 elers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
